e300cc0b4ffc12cfc86d18666ab7c07b055feae5
collnell <cnell@usgs.gov>
2022-01-08 17:43:07 -0600

    stashing

--- src/components/DragNDrop.vue
+++ src/components/DragNDrop.vue
@@ -10,12 +10,13 @@
         <arrows class="hwc-arrows" />
         <labels class="hwc-labels" />
         <blobs class="hwc-blobs" />
+        <svg class="word-bank" />
     </div>
   </section>
 </template>
 <script>
 import * as d3Base from 'd3';
-import {drag} from "d3-drag@3";
+import {drag} from "d3-drag";
 import arrows from "@/assets/images/human-water-cycle-testing2-arrows-01.svg";
 import labels from "@/assets/images/human-water-cycle-testing2-labels-01.svg";
 import blobs from "@/assets/images/blobs-01.svg";
@@ -35,6 +36,9 @@ export default {
       width: null,
       height: null,
       margin: { top: 50, right: 50, bottom: 50, left: 50 },
+      svg: null,
+      word_bank: null,
+      radius: null, 
 
     }
   },
@@ -42,15 +46,61 @@ export default {
       this.d3 = Object.assign(d3Base);
 
       // resize
-      this.width = window.innerWidth //- this.margin.left - this.margin.right;
-      this.height = window.innerHeight//*.5 - this.margin.top - this.margin.bottom;
+      this.width = 2736 //- this.margin.left - this.margin.right;
+      this.height = 1872//*.5 - this.margin.top - this.margin.bottom;
+      this.radius = 32;
 
-      // define style before page fully loads
-      this.svg = this.d3.select("#grid-container")
-        .append('svg')
-        .attr("viewBox", [0, 0, 2736, 1872])
+      this.word_bank = this.d3.select(".hwc-blobs")
+        .append('g')
+        .attr("viewBox", [0, 0, this.width, this.height])
+        .classed("word-bank-svg", true)
+
+      this.makeDrops();
+
+    
     },
     methods:{
+        makeDrops(){
+            const self = this
+
+            const circles = this.d3.range(10).map(i => ({
+                x: Math.random() * (this.width - this.radius * 2) + this.radius,
+                y: Math.random() * (this.height - this.radius * 2) + this.radius,
+            }));
+
+            this.word_bank.selectAll("circle")
+                .data(circles)
+                .join("circle")
+                .attr("cx", d => d.x)
+                .attr("cy", d => d.y)
+                .attr("r", this.radius)
+                .attr("fill", (d, i) => this.d3.schemeCategory10[i % 10])
+                .attr("id", (d, i) => { return 'dot_' + i })
+                .call((d, i) => self.drag(d, i));
+
+            return this.word_bank.node();
+
+        },
+        drag(d, i){
+            console.log(d, i)
+            function dragstarted(event, d) {
+                this.d3.select(this).raise().attr("stroke", "black");
+            }
+
+        function dragged(event, d) {
+                this.d3.select(this).attr("cx", d.x = event.x).attr("cy", d.y = event.y);
+            }
+
+        function dragended(event, d) {
+                this.d3.select(this).attr("stroke", null);
+            }
+
+        return this.d3.drag()
+            .on("start", dragstarted)
+            .on("drag", dragged)
+            .on("end", dragended);
+
+        }
     }
 }
 </script>
@@ -88,5 +138,15 @@ export default {
     grid-row: 1 / 2;
     grid-column: 2 / 2;
     z-index: 5;
+}
+.word-bank {
+    max-width: 100%;
+    max-height: 100%;
+    grid-row: 1 / 2;
+    grid-column: 2 / 2;
+    z-index: 6;
+}
+.word-bank-svg {
+    fill: blue;
 }
 </style>
