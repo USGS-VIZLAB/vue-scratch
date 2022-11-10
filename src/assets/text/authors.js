26ad98964585588366bc0a96c75d94070940331c
Corson-Dosch <hcorson-dosch@usgs.gov>
2022-11-10 16:01:31 -0600

    set up dynamic authorship component

---
+++ src/assets/text/authors.js
@@ -0,0 +1,78 @@
+export default {
+    primaryAuthors: [
+      {
+        firstName: 'Althea1',
+        lastName: 'Archer',
+        fullName: 'Althea Archer1',
+        initials: 'AA1',
+        profile_link: 'https://www.usgs.gov/staff-profiles/althea-archer',
+        role: 'lead developer',
+        contribution: 'led the design and development of the Vue website'
+      },
+      {
+        firstName: 'Althea2',
+        lastName: 'Archer',
+        fullName: 'Althea Archer2',
+        initials: 'AA2',
+        profile_link: 'https://www.usgs.gov/staff-profiles/althea-archer',
+        role: 'lead developer',
+        contribution: 'designed the illustrations'
+      },
+      {
+        firstName: 'Althea3',
+        lastName: 'Archer',
+        fullName: 'Althea Archer3',
+        initials: 'AA3',
+        profile_link: 'https://www.usgs.gov/staff-profiles/althea-archer',
+        role: 'lead developer',
+        contribution: 'made the charts'
+      }
+    ],
+    additionalAuthors: [
+      {
+        firstName: 'Cee',
+        lastName: 'Nell',
+        fullName: 'Cee Nell',
+        initials: 'CN',
+        profile_link: 'https://www.usgs.gov/staff-profiles/cee-nell',
+        role: 'supervisor',
+        contribution: 'acted in an avisory role and reviewed code'
+      },
+      {
+        firstName: 'Hayley',
+        lastName: 'Corson-Dosch',
+        fullName: 'Hayley Corson-Dosch',
+        initials: 'HCD',
+        profile_link: 'https://www.usgs.gov/staff-profiles/hayley-corson-dosch',
+        role: 'contributor',
+        contribution: 'built the initial pipeline and reviewed code'
+      },
+      {
+        firstName: 'Elmera',
+        lastName: 'Azadpour',
+        fullName: 'Elmera Azadpour',
+        initials: 'EA',
+        profile_link: 'https://www.usgs.gov/staff-profiles/elmera-azadpour',
+        role: 'critiquer',
+        contribution: 'provided design critique'
+      },
+      {
+        firstName: 'Lindsay',
+        lastName: 'Platt',
+        fullName: 'Lindsay Platt',
+        initials: 'LP',
+        profile_link: 'https://www.usgs.gov/staff-profiles/lindsay-rc-platt',
+        role: 'critiquer',
+        contribution: 'helped set up the svg'
+      },
+      {
+        firstName: 'Mandie',
+        lastName: 'Carr',
+        fullName: 'Mandie Carr',
+        initials: 'MC',
+        profile_link: 'https://www.usgs.gov/staff-profiles/amanda-carr',
+        role: 'contributor',
+        contribution: 'provided design critique'
+      }
+      ]
+};
